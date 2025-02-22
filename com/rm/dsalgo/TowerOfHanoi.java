ade37c93ede0914f0c9ec0c6203867d423285b6b
Ramit <ramit.mkj6@gmail.com>
2025-02-22 15:26:43 +0530

    tower of hanoi

---
+++ com/rm/dsalgo/TowerOfHanoi.java
@@ -0,0 +1,26 @@
+package com.rm.dsalgo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TowerOfHanoi {
+
+    public static void main(String[] args) {
+        int n = 4;
+        List<String> steps = new LinkedList<String>();
+        TowerOfHanoi.hanoi(n, "S", "E", "A", steps);
+
+        steps.forEach(System.out::println);
+        System.out.println("Minimum Steps: " + steps.size());
+    }
+
+    public static void hanoi(int n, String start, String end, String aux, List<String> steps) {
+        if(n == 0) {
+            return;
+        }
+        hanoi(n-1, start, aux, end, steps);
+        steps.add(start + " -> " + end);
+        hanoi(n-1, aux, end, start, steps);
+    }
+
+}
